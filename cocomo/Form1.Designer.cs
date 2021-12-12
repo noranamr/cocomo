ce40a6b4aba8d20bb89d2765242501dda61cf8d8
Khadijatarek <khadijaamer01@gmail.com>
2021-12-12 23:17:10 +0200

    added button

--- cocomo/Form1.Designer.cs
+++ cocomo/Form1.Designer.cs
@@ -30,22 +30,33 @@ namespace cocomo
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(389, 108);
+            this.label1.Location = new System.Drawing.Point(438, 113);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 20);
+            this.label1.Size = new System.Drawing.Size(44, 21);
             this.label1.TabIndex = 0;
             this.label1.Text = "hiiiii!";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(465, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "HEYYYYYYY";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(900, 472);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +68,7 @@ namespace cocomo
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
