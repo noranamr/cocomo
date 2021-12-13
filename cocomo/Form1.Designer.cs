3721fc025865056534aec2a06f8cdbcebefd2256
khadijakhaled <85907583+khadijakhaled@users.noreply.github.com>
2021-12-13 16:04:36 +0200

    added a label

--- cocomo/Form1.Designer.cs
+++ cocomo/Form1.Designer.cs
@@ -31,33 +31,47 @@ namespace cocomo
         {
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(438, 113);
+            this.label1.Location = new System.Drawing.Point(341, 81);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 21);
+            this.label1.Size = new System.Drawing.Size(32, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "hiiiii!";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(465, 229);
+            this.button1.Location = new System.Drawing.Point(362, 164);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(158, 53);
+            this.button1.Size = new System.Drawing.Size(123, 38);
             this.button1.TabIndex = 1;
             this.button1.Text = "HEYYYYYYY";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(430, 296);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hello";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(900, 472);
+            this.ClientSize = new System.Drawing.Size(700, 337);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -69,6 +83,7 @@ namespace cocomo
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
